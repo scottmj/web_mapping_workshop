2d599413b4407b6032bac9f19480a4f71518bba1
scottmj <scott.jackson087@gmail.com>
2016-05-02 15:53:56 -0400

    added overlay

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -42,9 +42,9 @@ var clickHandler = function(e){
     	var info = '';
       	info += '<div>';
       	info += 'h2' + feature.properties.name + '<h2>';
-      	if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '<p>';
-      	if(feature.properties.phone) info += '<p>' + feature.properties.phone + '<p>';
-      	if(feature.properties.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a><p>';
+      	//if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '<p>';
+      	//if(feature.properties.phone) info += '<p>' + feature.properties.phone + '<p>';
+      	//if(feature.properties.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a><p>';
       	info += '</div>';
     
     $('info').append(info);
