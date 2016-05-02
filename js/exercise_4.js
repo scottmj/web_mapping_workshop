203138e1510f618d896246e7c3a46fe3b1aae1e5
scottmj <scott.jackson087@gmail.com>
2016-05-02 15:22:56 -0400

    added popup

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -32,6 +32,12 @@ featureLayer.on('ready', function(){
           	"marker-symbol": "restaurant"
         }))
     })
-    map.fitBounds(featureLayer.getBounds());    
+    map.fitBounds(featureLayer.getBounds()); 
+})
+
+featureLayer.on('ready', function(){
+	this.eachLayer(function(layer){
+    	layer.bindPopup('Welcome to ' + layer.feature.properties.name)
+    })
 })
 
