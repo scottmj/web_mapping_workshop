a8e314cf05ae1e7301f5fa6151beda8f17ec0e46
scottmj <scott.jackson087@gmail.com>
2016-05-02 15:47:49 -0400

    added overlay

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -35,11 +35,32 @@ featureLayer.on('ready', function(){
     map.fitBounds(featureLayer.getBounds()); 
 })
 
+var clickHandler = function(e){
+	$('#info').empty();
+  	var feature = e.target.feature;
+  	$('#sidebar').fadeIn(400, function(){
+    	var info = '';
+      	info += '<div>';
+      	info += 'h2' + features.properties.name + '<h2>';
+      	if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '<p>';
+      	if(feature.properties.phone) info += '<p>' + feature.properties.phone + '<p>';
+      	if(feature.properties.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a><p>';
+      	info += '</div>';
+    
+    $('info').apped(info);
+    });
+}
+
 featureLayer.on('ready', function(){
 	this.eachLayer(function(layer){
-    	layer.bindPopup(layer.feature.properties.name)
+      	layer.on('click', clickHandler);
     })
 })
+//featureLayer.on('ready', function(){
+//	this.eachLayer(function(layer){
+//    	layer.bindPopup(layer.feature.properties.name)
+//    })
+//})
 
 
 //"addr:city": "Durham",
