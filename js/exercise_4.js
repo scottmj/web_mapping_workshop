5125e534cbefe18955e9bb968d72bfdaf205419a
scottmj <scott.jackson087@gmail.com>
2016-05-02 17:02:47 -0400

    added route

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -39,15 +39,17 @@ var clickHandler = function(e){
   	var feature = e.target.feature;
   	$('#sidebar').fadeIn(400, function(){
     	var info = '';
-      	info += '<div>';
-      	info += '<h2>' + feature.properties.name + '<h2>';
-      	if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '<p>';
-      	if(feature.properties.phone) info += '<p>' + feature.properties.phone + '<p>';
-      	if(feature.properties.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a><p>';
-      	info += '</div>';
-    console.log(info);
-    $('#info').append(info);
-    });
+      		info += '<div>';
+      		info += '<h2>' + feature.properties.name + '<h2>';
+      		if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '<p>';
+      		if(feature.properties.phone) info += '<p>' + feature.properties.phone + '<p>';
+      		if(feature.properties.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a><p>';
+      		info += '</div>';
+    	$('#info').append(info);
+    })
+    var myGeoJSON = myLocation.getGeoJSON();
+  	getDirections(myGeoJSON.geometry.coordinates, feature.geometry.coordinates);
+    
 }
 
 featureLayer.on('ready', function(){
@@ -78,6 +80,40 @@ map.on('locationfound'), function(e){
 }
 
 map.locate({setView: true})
+
+var routeLine = L.mapbox.featureLayer().addTo(map);
+
+function getDirections(frm, to){
+	var jsonPayload = JSON.stringify({
+    	locations: [
+        	{lat: frm[1], lon: frm[0]},
+          	{lat: to[1], lon: to[0]}
+        ],
+      	costing: 'pedestrian',
+      	units: 'miles'
+    })
+    $.ajax({
+    	url: 'http://valhalla.mapzen.com/routes',
+      	data: {
+        	json: jsonPayload,
+          	api_key: 'valhalla-gwtf3x2' //will only work during conf, register with mapzen to get my own
+        }
+    }).done(function(data){
+    	var routeShape = polyline.decode(data.trip.legs[0].shape);
+      	routeLine.setGeoJSON({
+        	type: 'Feature',
+          	geometry: {
+            	type: 'LineString',
+              	coordinates: routeShape
+            },
+          	properties: {
+            	"stroke": "#ed23f1",
+              	"stroke-opacity": 0.8,
+              	"stroke-width": 8
+            }
+        })
+    })
+}
 
 
 //featureLayer.on('ready', function(){
